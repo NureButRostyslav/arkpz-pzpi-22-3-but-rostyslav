a88e8c8e8b78cf513518ef5d302b4d31b55d5990
But Rostyslav <rostyslav.but@nure.ua>
2025-05-24 18:03:05 +0300

    Update serializers.py

--- Task3/arkpz-pzpi-22-3-but-rostyslav-task3/api/serializers.py
+++ Task3/arkpz-pzpi-22-3-but-rostyslav-task3/api/serializers.py
@@ -1,1 +1,31 @@
+from rest_framework import serializers
+from .models import UserProfile, Resource, Expense, Budget, ActionLog
+from django.contrib.auth.models import User
 
+class UserProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    email = serializers.EmailField(source='user.email', read_only=True)
+
+    class Meta:
+        model = UserProfile
+        fields = ['id', 'username', 'email', 'corporate_account_id']
+
+class ResourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Resource
+        fields = ['id', 'name', 'cost_per_hour']
+
+class ExpenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Expense
+        fields = ['id', 'user', 'resource', 'start_time', 'end_time', 'total_cost']
+
+class BudgetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Budget
+        fields = ['id', 'corporate_account_id', 'limit_amount']
+
+class ActionLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ActionLog
+        fields = ['id', 'admin', 'action', 'timestamp']
