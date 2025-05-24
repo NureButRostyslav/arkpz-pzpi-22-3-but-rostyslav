5f83e47043efc5ef1b6ff05384cc58adc3198c6e
But Rostyslav <rostyslav.but@nure.ua>
2025-05-24 18:02:47 +0300

    Update permissions.py

--- Task3/arkpz-pzpi-22-3-but-rostyslav-task3/api/permissions.py
+++ Task3/arkpz-pzpi-22-3-but-rostyslav-task3/api/permissions.py
@@ -1,1 +1,10 @@
+from rest_framework.permissions import BasePermission
+
+class IsAdminUserOnly(BasePermission):
+    """
+    Дозволяє доступ лише адміністраторам.
+    """
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.is_staff
 
